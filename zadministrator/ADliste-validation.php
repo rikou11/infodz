5709f198d61d9c0075f02b133986cd0fec3814f0
fares <fares.bensaad@univ-constantine2.dz>
2022-01-16 02:07:52 +0100

    comit

--- zadministrator/ADliste-validation.php
+++ zadministrator/ADliste-validation.php
@@ -125,7 +125,7 @@ include("php/tableau-validation.php");
                   </button>
 
 
-<div class="modal fade" id="' . $row["id"] . 'bloque" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal fade" id="' . $row["id"] . 'bloque" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
 <div class="modal-dialog modal-dialog-centered" role="document">
   <div class="modal-content">
     <div class="modal-header">
